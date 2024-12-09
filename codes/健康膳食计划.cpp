1d43e22e5d6525ef8277b6296de181795d9df97f
baiyei_dk <1211050111@qq.com>
2024-12-09 22:49:05 +0800

    取出膳食计划的类使用

--- codes/健康膳食计划.cpp
+++ codes/健康膳食计划.cpp
@@ -1,133 +1,105 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 优化输入输出
-static const auto io_sync_off = []()
+const int MAX_N = 16;
+const int MAX_M = 65536;
+
+vector<unsigned short> foods;
+unsigned short target;
+int n, m;
+
+vector<int> dp;
+vector<int> prev_state;
+vector<int> choice;
+
+void read_input()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
+    cin >> n >> m;
+    int temp;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> temp;
+    }
 
-class Solution
+    target = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        target |= (1 << temp);
+    }
+
+    foods.resize(m, 0);
+    for (int i = 0; i < m; i++)
+    {
+        string line;
+        getline(cin >> ws, line);
+        istringstream iss(line);
+        while (iss >> temp)
+        {
+            foods[i] |= (1 << temp);
+        }
+    }
+}
+
+bool process()
 {
-private:
-    static const int MAX_N = 16;
-    static const int MAX_M = 65536;
-    vector<unsigned short> foods; // 每种食物包含的营养素状态
-    unsigned short target;        // 目标营养素状态
-    int n, m;                     // n: 营养素数量, m: 食物数量
+    int total_states = 1 << MAX_N;
+    dp.assign(total_states, INT_MAX);
+    prev_state.assign(total_states, -1);
+    choice.assign(total_states, -1);
+    dp[0] = 0;
 
-    // DP数组
-    vector<int> dp;     // dp[s]表示达到状态s所需的最少食物数量
-    vector<int> prev;   // prev[s]记录达到状态s时上一个状态
-    vector<int> choice; // choice[s]记录达到状态s时选择的食物
-
-public:
-    void solve()
+    for (int state = 0; state < total_states; state++)
     {
-        read_input();
-        if (process())
-        {
-            print_result();
-        }
-        else
-        {
-            cout << -1 << endl;
-        }
-    }
+        if (dp[state] == INT_MAX)
+            continue;
 
-private:
-    void read_input()
-    {
-        cin >> n >> m;
-        int temp;
-        for (int i = 0; i < m; i++)
+        for (int j = 0; j < m; j++)
         {
-            cin >> temp; // 读取但不使用每种食物的营养素数量
-        }
-
-        // 读取目标营养素列表
-        target = 0;
-        for (int i = 0; i < n; i++)
-        {
-            cin >> temp;
-            target |= (1 << temp);
-        }
-
-        // 读取每种食物的营养素列表
-        foods.resize(m, 0);
-        for (int i = 0; i < m; i++)
-        {
-            string line;
-            getline(cin >> ws, line);
-            istringstream iss(line);
-            while (iss >> temp)
+            int new_state = state | foods[j];
+            if (dp[state] + 1 < dp[new_state])
             {
-                foods[i] |= (1 << temp);
+                dp[new_state] = dp[state] + 1;
+                prev_state[new_state] = state;
+                choice[new_state] = j;
             }
         }
     }
 
-    bool process()
+    return dp[target] != INT_MAX;
+}
+
+void print_result()
+{
+    vector<int> result;
+    unsigned short current_state = target;
+
+    while (current_state != 0)
     {
-        int total_states = 1 << MAX_N;
-        dp.assign(total_states, INT_MAX);
-        prev.assign(total_states, -1);
-        choice.assign(total_states, -1);
-        dp[0] = 0;
-
-        // 遍历所有可能的状态
-        for (int state = 0; state < total_states; state++)
-        {
-            if (dp[state] == INT_MAX)
-                continue;
-
-            // 尝试每种食物
-            for (int j = 0; j < m; j++)
-            {
-                int new_state = state | foods[j];
-                if (dp[state] + 1 < dp[new_state])
-                {
-                    dp[new_state] = dp[state] + 1;
-                    prev[new_state] = state;
-                    choice[new_state] = j;
-                }
-            }
-        }
-
-        return dp[target] != INT_MAX;
+        result.push_back(choice[current_state]);
+        current_state = prev_state[current_state];
     }
 
-    void print_result()
+    sort(result.begin(), result.end());
+
+    for (int i = 0; i < result.size(); i++)
     {
-        vector<int> result;
-        unsigned short current_state = target;
-
-        while (current_state != 0)
-        {
-            result.push_back(choice[current_state]);
-            current_state = prev[current_state];
-        }
-
-        sort(result.begin(), result.end());
-
-        for (int i = 0; i < result.size(); i++)
-        {
-            cout << result[i];
-            if (i < result.size() - 1)
-            {
-                cout << " ";
-            }
-        }
-        cout << endl;
+        cout << result[i];
+        if (i < result.size() - 1)
+            cout << " ";
     }
-};
+    cout << endl;
+}
 
 int main()
 {
-    Solution solution;
-    solution.solve();
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    read_input();
+    if (process())
+    {
+        print_result();
+    }
+
     return 0;
 }
